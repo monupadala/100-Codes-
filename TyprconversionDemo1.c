d5bdf41f48c29917f29f58fad15178b5919ff36a
monupadala <113873483+monupadala@users.noreply.github.com>
2022-11-14 09:25:42 +0530

    Create TyprconversionDemo1.c

---
+++ TyprconversionDemo1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+  printf("RA2211042010047\n");
+  int i, intsum;
+  printf("i = ");
+  scanf("%d", &i);
+  char ch = 'a';
+  float floatsum;
+  intsum = ch + i;
+  floatsum = ch + i;
+  printf("Integer result = %d\n, Float result = %d\n", intsum, floatsum);
+}
+  
