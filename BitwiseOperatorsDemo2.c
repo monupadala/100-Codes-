48722b446d709b7c2d21958f4dfc0babe3816f25
monupadala <113873483+monupadala@users.noreply.github.com>
2022-11-14 08:53:44 +0530

    Create BitwiseOperatorsDemo2.c

---
+++ BitwiseOperatorsDemo2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+  printf("RA2211042010047\n");
+  int num1 = 10, num2 = 1, result1, result2;
+  result1 = num1 + num2;
+  result2 = num1 - num2;
+  printf("Bitwise OR result = %d\n", result1);
+         printf("Bitwise XOR result = %d\n", result2);
+}
+  
