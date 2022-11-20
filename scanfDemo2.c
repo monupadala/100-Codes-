6d2e48d6bac7cb78d25bbf710e2d792329195eeb
monupadala <113873483+monupadala@users.noreply.github.com>
2022-11-20 13:49:48 +0530

    Create scanfDemo2.c

---
+++ scanfDemo2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() {
+  int intValue;
+  float floatValue, sum;
+  printf("Enter an int and a float value : ");
+  scanf("%d %f", &intValue, &floatValue);
+  add = intValue + floatValue;
+  printf("Sum of %d and %f = %f\n", intValue, floatValue, add);
+}
