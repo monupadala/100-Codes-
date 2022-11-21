f9d6a65851b1e4816e4fcd22a760fb18d55b980d
monupadala <113873483+monupadala@users.noreply.github.com>
2022-11-21 09:53:41 +0530

    Create IfElseDemo1.c

---
+++ IfElseDemo1.c
@@ -0,0 +1,14 @@
+#include<<stdio.h>
+void main() {
+  printf("RA2211042010047");
+  int marks, distinction_marks = 75;
+  printf("Enter marks :");
+  scanf("%d", &marks);
+  if (marks > 75) {
+    printf("User secured distinction");
+    }
+    else 
+    {
+      printf("User did not secured 75");
+    }
+}
