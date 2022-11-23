c29f0beb42ac44c048246d2a72edb764fd004c4e
monupadala <113873483+monupadala@users.noreply.github.com>
2022-11-23 21:15:44 +0530

    Update ++ operator.c

--- ++ operator.c
+++ ++ operator.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 int main() 
 {
-  printf("Register number: RA2211042010007\n");
+  printf("Register number: RA2211042010047\n");
   int x = 5;
   printf("%d", ++x);
   return 0;
